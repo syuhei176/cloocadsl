1d84266072abc42d040e30b4e7ecdea92b76e694
syuhei176 <syuhei176@gmail.com>
2012-03-18 06:21:43 +0900

    pathを追加

--- cloocadsl/src/com/clooca/core/client/model/gopr/metaelement/GraphicInfo.java
+++ cloocadsl/src/com/clooca/core/client/model/gopr/metaelement/GraphicInfo.java
@@ -1,5 +1,10 @@
 package com.clooca.core.client.model.gopr.metaelement;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.clooca.core.client.util.Point2D;
+
 public class GraphicInfo {
 	public int id;
 	public String shape;
@@ -7,4 +12,6 @@ public class GraphicInfo {
 	public static final String RECT = "rect";
 	public static final String ROUNDED = "rounded";
 	public static final String CIRCLE = "circle";
+	public static final String PATH = "path";
+	public List<Point2D> path = new ArrayList<Point2D>();
 }
