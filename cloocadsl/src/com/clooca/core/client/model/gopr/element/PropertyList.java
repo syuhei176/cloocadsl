5af6880485eab6c08c84006f84ed731fd8b68ca8
syuhei176 <syuhei176@gmail.com>
2012-03-30 16:32:11 +0900

    PropertyList追加

---
+++ cloocadsl/src/com/clooca/core/client/model/gopr/element/PropertyList.java
@@ -0,0 +1,13 @@
+package com.clooca.core.client.model.gopr.element;
+
+import java.util.ArrayList;
+
+import com.clooca.core.client.model.gopr.metaelement.MetaProperty;
+
+public class PropertyList extends ArrayList<Property> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4541196834310609442L;
+	public MetaProperty meta;
+}
