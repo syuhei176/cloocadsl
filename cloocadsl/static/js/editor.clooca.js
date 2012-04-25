e31769252aedd4eeeb32aa27f73b6fe94921f0f8
syuhei176 <syuhei176@gmail.com>
2012-04-26 00:52:43 +0900

    ソースを動的ロードするようにした

---
+++ cloocadsl/static/js/editor.clooca.js
@@ -0,0 +1,31 @@
+function clooca() {}
+
+clooca.include = function(fn) {
+    var script = document.createElement( 'script' );
+    script.type = 'text/javascript';
+    script.src = "/static/js/"+fn;
+    $("#script-div").append( script );
+}
+
+clooca.includes = function(fs) {
+	for(var i=0;i < fs.length;i++) {
+    	var script = document.createElement( 'script' );
+    	script.type = 'text/javascript';
+    	script.src = "/static/js/"+fs[i];
+    	$("#script-div").append( script );
+	}
+}
+
+function initall() {
+	clooca.includes(['dev/core/math2d.js',
+	                 'dev/core/controller.js',
+	                 'dev/core/arrowhead.js',
+	                 'dev/core/DiagramController.js',
+	                 'dev/core/DiagramEditor.js',
+	                 'dev/core/metamodel.js',
+	                 'dev/core/model.js',
+	                 'dev/core/EditorTabPanel.js',
+	                 'dev/editor/controller.js',
+	                 'dev/editor/editor.js',
+	                 'dev/editor/view.js']);
+}
