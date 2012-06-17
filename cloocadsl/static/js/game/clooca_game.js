3821a722595a855bc4ea2e4a827dba2fe6cde6ac
syuhei176 <syuhei176@gmail.com>
2012-06-17 15:02:14 +0900

    add

---
+++ cloocadsl/static/js/game/clooca_game.js
@@ -0,0 +1,51 @@
+function load_mycharacters(success_cb, error_cb) {
+    $.ajax({
+        type: "GET",
+        url: "/mycharacters",
+        data: {},
+        dataType: 'json',
+        success: function(data, dataType) { /** Ajax通信が成功した場合に呼び出される */
+        	success_cb(data);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {  /** Ajax通信が失敗した場合に呼び出される */
+                this; // thisは他のコールバック関数同様にAJAX通信時のオプションを示します。
+                error_cb(errorThrown);
+//                alert('Error : ' + errorThrown);
+        }
+    });
+}
+
+function load_characters(success_cb, error_cb) {
+    $.ajax({
+        type: "GET",
+        url: "/characters",
+        data: {},
+        dataType: 'json',
+        success: function(data, dataType) { /** Ajax通信が成功した場合に呼び出される */
+        	success_cb(data);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {  /** Ajax通信が失敗した場合に呼び出される */
+                this; // thisは他のコールバック関数同様にAJAX通信時のオプションを示します。
+                error_cb(errorThrown);
+//                alert('Error : ' + errorThrown);
+        }
+    });
+}
+
+
+function gen_chara(id, success_cb, error_cb) {
+    $.ajax({
+        type: "POST",
+        url: "/gen-chara",
+        data: {id : id},
+        dataType: 'json',
+        success: function(data, dataType) { /** Ajax通信が成功した場合に呼び出される */
+        	success_cb(data);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {  /** Ajax通信が失敗した場合に呼び出される */
+                this; // thisは他のコールバック関数同様にAJAX通信時のオプションを示します。
+                error_cb(errorThrown);
+//                alert('Error : ' + errorThrown);
+        }
+    });
+}
